b53fa39208b35ba696b2bfd308559d41cf9082b5
marloxxx <66661134+marloxxx@users.noreply.github.com>
2024-06-05 22:29:42 +0700

    update migration and controller, remove nik

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -27,7 +27,6 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'nik' => 'required|numeric|digits_between:16,16|unique:users',
             'name' => 'required|string|min:3|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
@@ -110,8 +109,6 @@ class AuthController extends Controller
             'user' => $user
         ], 'User Logged In');
     }
-
-
 
     /**
      * Forgot Password
