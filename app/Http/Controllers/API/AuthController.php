5c0612df1ace9ea25c57fd61b9b21bd4a6733647
marloxxx <66661134+marloxxx@users.noreply.github.com>
2024-06-06 00:08:40 +0700

    Update AuthController.php

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|min:3|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
+            'password' => 'required|string|min:8',
             'phone' => 'numeric|digits_between:10,13',
         ]);
 
